5e22d3f14cb085258d7d1ed79b22f169eac617c4
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-04-30 12:27:28 +0200

    Update the project

---
+++ client/build/precache-manifest.7664a57c1446ec9c2ea9db68930ca2a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f94d91ea381d5c6088cd17eca2d6f51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef37d29dbeecd503e200",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "511b1589814fc66890ba",
+    "url": "/static/css/main.15c64e45.chunk.css"
+  },
+  {
+    "revision": "ef37d29dbeecd503e200",
+    "url": "/static/js/2.339f3c1a.chunk.js"
+  },
+  {
+    "revision": "511b1589814fc66890ba",
+    "url": "/static/js/main.667d6b9a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
